77ea556f396f9ae5d0b6b43fdf0d5cad0226b603
Yehia Abdelbasier <yehiafml20333@gmail.com>
2021-03-07 14:37:52 +0200

    flutter

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
@@ -12,187 +12,193 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Tutorial',
-      home: _SizedBoxExample(),
+      title: 'معاهد أبوقير العليا',
+      home: MyApp2(),
     );
   }
 }
 
-class _SizedBoxExample extends StatelessWidget {
+// _launchURL() async {
+//   final Uri uri = Uri(
+//     scheme: 'https',
+//     path: 'www.dahab-informatics.com/test',
+//     queryParameters: {'name': 'dahab-informatics', 'about': 'Flutter Dart'},
+//   );
+
+//   if (await canLaunch(uri.toString())) {
+//     await launch(
+//       uri.toString(),
+//       forceWebView: true,
+//       enableJavaScript: true,
+//       enableDomStorage: true,
+//     );
+//   } else {
+//     print('Could not launch url');
+//   }
+// }
+
+class MyInAppBrowser extends InAppBrowser {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('المعهد العالي للحاسب الالي',
-              style: TextStyle(fontWeight: FontWeight.bold)),
-          centerTitle: true,
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/banner4.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Center(
-              child: Column(children: [
-            SizedBox(height: 15),
-            Image.asset('assets/images/logo.png'),
-            Text('وزارة التعليم العالي',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text('معاهد أبوقير العليا',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(height: 15),
-            Image.asset('assets/images/CISLOGO.png'),
-            SizedBox(height: 15),
-            RaisedButton(onPressed: _launchURL, child: Text('Go')),
-            SizedBox(height: 15),
-            Image.asset('assets/images/HITHR.png'),
-            SizedBox(height: 15),
-            RaisedButton(onPressed: _launchURL, child: Text('Go')),
-            SizedBox(height: 15),
-            RaisedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyApp2()),
-                  );
-                },
-                child: Text('Go'))
-          ])),
-        ));
+  Future onBrowserCreated() async {
+    print("\n\nBrowser Created!\n\n");
   }
-}
 
-_launchURL() async {
-  final Uri uri = Uri(
-    scheme: 'https',
-    path: 'www.dahab-informatics.com/test',
-    queryParameters: {'name': 'dahab-informatics', 'about': 'Flutter Dart'},
-  );
+  @override
+  Future onLoadStart(String url) async {
+    print("\n\nStarted $url\n\n");
+  }
 
-  if (await canLaunch(uri.toString())) {
-    await launch(
-      uri.toString(),
-      forceWebView: true,
-      enableJavaScript: true,
-      enableDomStorage: true,
-    );
-  } else {
-    print('Could not launch url');
+  @override
+  Future onLoadStop(String url) async {
+    print("\n\nStopped $url\n\n");
+  }
+
+  @override
+  void onLoadError(String url, int code, String message) {
+    print("Can't load $url.. Error: $message");
+  }
+
+  @override
+  void onProgressChanged(int progress) {
+    print("Progress: $progress");
+  }
+
+  @override
+  void onExit() {
+    print("\n\nBrowser closed!\n\n");
+  }
+
+  @override
+  Future<ShouldOverrideUrlLoadingAction> shouldOverrideUrlLoading(
+      ShouldOverrideUrlLoadingRequest shouldOverrideUrlLoadingRequest) async {
+    print("\n\n override ${shouldOverrideUrlLoadingRequest.url}\n\n");
+    return ShouldOverrideUrlLoadingAction.ALLOW;
+  }
+
+  @override
+  void onLoadResource(LoadedResource response) {
+    print("Started at: " +
+        response.startTime.toString() +
+        "ms ---> duration: " +
+        response.duration.toString() +
+        "ms " +
+        response.url);
+  }
+
+  @override
+  void onConsoleMessage(ConsoleMessage consoleMessage) {
+    print("""
+    console output:
+      message: ${consoleMessage.message}
+      messageLevel: ${consoleMessage.messageLevel.toValue()}
+   """);
   }
 }
 
 class MyApp2 extends StatefulWidget {
+  final MyInAppBrowser browser = new MyInAppBrowser();
+
   @override
   _MyAppState createState() => new _MyAppState();
 }
 
 class _MyAppState extends State<MyApp2> {
-  InAppWebViewController webView;
-  String url = "";
-  double progress = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('InAppWebView Example'),
+          title: Text('معاهد أبوقير العليا',
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          centerTitle: true,
+          backgroundColor: Colors.black,
         ),
         body: Container(
-            child: Column(children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Text(
-                "CURRENT URL\n${(url.length > 50) ? url.substring(0, 50) + "..." : url}"),
-          ),
-          Container(
-              padding: EdgeInsets.all(10.0),
-              child: progress < 1.0
-                  ? LinearProgressIndicator(value: progress)
-                  : Container()),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.all(10.0),
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-              child: InAppWebView(
-                initialUrl: "https://flutter.dev/",
-                initialHeaders: {},
-                initialOptions: InAppWebViewGroupOptions(
-                    crossPlatform: InAppWebViewOptions(
-                  debuggingEnabled: true,
-                )),
-                onWebViewCreated: (InAppWebViewController controller) {
-                  webView = controller;
-                },
-                onLoadStart: (InAppWebViewController controller, String url) {
-                  setState(() {
-                    this.url = url;
-                  });
-                },
-                onLoadStop:
-                    (InAppWebViewController controller, String url) async {
-                  setState(() {
-                    this.url = url;
-                  });
-                },
-                onProgressChanged:
-                    (InAppWebViewController controller, int progress) {
-                  setState(() {
-                    this.progress = progress / 100;
-                  });
-                },
-              ),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/bookwhite.jpg"),
+              fit: BoxFit.cover,
             ),
           ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                child: Icon(Icons.arrow_back),
-                onPressed: () {
-                  if (webView != null) {
-                    webView.goBack();
-                  }
-                },
+          child: Center(
+              child: Column(children: [
+            SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                widget.browser.openUrl(
+                    url: "https://aboukir-institutes.edu.eg/",
+                    options: InAppBrowserClassOptions(
+                        inAppWebViewGroupOptions: InAppWebViewGroupOptions(
+                            crossPlatform: InAppWebViewOptions(
+                      useShouldOverrideUrlLoading: true,
+                      useOnLoadResource: true,
+                    ))));
+              },
+              child: Image.asset(
+                'assets/images/151.png',
               ),
-              RaisedButton(
-                child: Icon(Icons.arrow_forward),
-                onPressed: () {
-                  if (webView != null) {
-                    webView.goForward();
-                  }
-                },
+            ),
+            SizedBox(height: 15),
+            GestureDetector(
+              onTap: () {
+                widget.browser.openUrl(
+                    url:
+                        "https://quizcis.aboukir-institutes.com/login/index.php",
+                    options: InAppBrowserClassOptions(
+                        inAppWebViewGroupOptions: InAppWebViewGroupOptions(
+                            crossPlatform: InAppWebViewOptions(
+                      useShouldOverrideUrlLoading: true,
+                      useOnLoadResource: true,
+                    ))));
+              },
+              child: Image.asset(
+                'assets/images/HICIS.png',
+                width: 350,
               ),
-              RaisedButton(
-                child: Icon(Icons.refresh),
-                onPressed: () {
-                  if (webView != null) {
-                    webView.reload();
-                  }
-                },
+            ),
+            SizedBox(height: 15),
+            GestureDetector(
+              onTap: () {
+                widget.browser.openUrl(
+                    url:
+                        "https://quiztour.aboukir-institutes.com/login/index.php",
+                    options: InAppBrowserClassOptions(
+                        inAppWebViewGroupOptions: InAppWebViewGroupOptions(
+                            crossPlatform: InAppWebViewOptions(
+                      useShouldOverrideUrlLoading: true,
+                      useOnLoadResource: true,
+                    ))));
+              },
+              child: Image.asset(
+                'assets/images/HITR.png',
+                width: 350,
               ),
-              RaisedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('Go'))
-            ],
-          ),
-        ])),
-      ),
-    );
+            ),
+            SizedBox(height: 50),
+            Text(
+              'E-mail : info@aboukir-institutes.edu.eg',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 15),
+            Text(
+              'الرقم المختصر : 15549',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Telephone number : +203/5625241',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Telephone number 2 : +203/5624777',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Telephone number 3 : +203/5613070',
+              style: TextStyle(color: Colors.white),
+            ),
+          ])),
+        ));
   }
 }
